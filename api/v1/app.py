fdc4858d35096086a9c4fc7b2bea98a570c261bb
Ella711 <allenro711@gmail.com>
2022-05-30 17:49:46 -0500

    [ADD] - File for task6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ module to run flask app """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from os import getenv
 
@@ -24,6 +24,13 @@ def port_host(HBNB_API_HOST, HBNB_API_PORT):
 def teardown(exception):
     """ handle app teardowns with storage close """
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(self):
+    """ handle error_404 """
+    error_dict = {"error": "Not found"}
+    return make_response(jsonify(error_dict), 404)
 
 
 if __name__ == "__main__":
