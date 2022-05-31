f5a12499b3e69e341e6a3532ea6144e18fc7ed47
Ella711 <allenro711@gmail.com>
2022-05-31 12:54:47 -0500

    [ADD] - Files for task9

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -3,8 +3,6 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
 from models import storage
-from models.city import City
-from models.state import State
 from models.amenity import Amenity
 
 
